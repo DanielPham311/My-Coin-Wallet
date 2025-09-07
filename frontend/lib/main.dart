39524068fa02cc183a090597f89049f54fb78a7a
unknown <phamquocduy3112003@gmail.com>
2025-09-07 22:10:21 +0700

    Revert "Merge pull request #5 from DanielPham311/FEwork"
    This reverts commit 9e8bbeff574a4bded2e52263e700a836048c7e00, reversing
    changes made to 644fd46700d505dbf2bb5e3b50c3e0b0d5be5e0d.

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/pages/login.dart';
-import 'package:frontend/pages/register.dart';
-import 'package:frontend/pages/wallet_home.dart';
 import 'pages/create_wallet.dart';
 import 'pages/dashboard.dart';
 import 'pages/send_page.dart';
 
-void main() => runApp(const MyCoinApp());
+void main() => runApp(MyCoinApp());
 
 class MyCoinApp extends StatelessWidget {
   const MyCoinApp({super.key});
@@ -15,18 +12,14 @@ class MyCoinApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MyCoin Wallet',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.indigo),
-      initialRoute: '/login',
+      initialRoute: '/',
       routes: {
-        '/': (context) => const CreateWalletPage(),
-        '/login': (context) => const LoginScreen(),
-        '/register': (context) => const RegisterScreen(),
-        '/wallet': (context) => const WalletHome(),
+        '/': (context) => CreateWalletPage(),
         '/dashboard': (context) => DashboardPage(),
         '/send': (context) {
-          final args = ModalRoute.of(context)!.settings.arguments
-              as Map<String, String>;
+          final args =
+              ModalRoute.of(context)!.settings.arguments as Map<String, String>;
           return SendPage(
             address: args['address']!,
             privateKey: args['privateKey']!,
